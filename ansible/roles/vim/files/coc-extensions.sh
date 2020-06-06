154719be49470e4c73b593b69035551f92a63f8e
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2020-06-06 16:46:49 -0400

    Replacing coc lsp with native neovim lsp

--- ansible/roles/vim/files/coc-extensions.sh
+++ ansible/roles/vim/files/coc-extensions.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-
-set -o nounset    # error when referencing undefined variable
-set -o errexit    # exit when command fails
-
-# Install extensions
-mkdir -p ~/.config/coc/extensions
-cd ~/.config/coc/extensions
-if [ ! -f package.json ]
-then
-  echo '{"dependencies":{}}'> package.json
-fi
-# Change extension names to the extensions you need
-npm install coc-json coc-tsserver --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+npm install json tsserver --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
---
+++ ansible/roles/vim/files/install-lsp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+npm install -g neovim typescript-language-server --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
