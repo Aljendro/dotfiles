ffd2d7c4a416b0f3416d187134aab8fcc0e803e0
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2020-06-06 00:30:33 -0400

    Adding coc extensions

---
+++ ansible/roles/vim/files/coc-extensions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o nounset    # error when referencing undefined variable
+set -o errexit    # exit when command fails
+
+# Install extensions
+mkdir -p ~/.config/coc/extensions
+cd ~/.config/coc/extensions
+if [ ! -f package.json ]
+then
+  echo '{"dependencies":{}}'> package.json
+fi
+# Change extension names to the extensions you need
+npm install coc-json coc-tsserver --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
