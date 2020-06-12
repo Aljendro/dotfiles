5b6900a2a6777ae9c8a3a40db1c8b88a9429340c
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2020-06-11 21:06:28 -0400

    Installing coc-rls for rust support

--- ansible/roles/vim/files/install-lsp.sh
+++ ansible/roles/vim/files/install-lsp.sh
@@ -7,5 +7,5 @@ then
   echo '{"dependencies":{}}'> package.json
 fi
 # Change extension names to the extensions you need
-npm install coc-json coc-tsserver coc-vimlsp --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+npm install coc-json coc-tsserver coc-vimlsp coc-rls --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 
