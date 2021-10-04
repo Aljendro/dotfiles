81ea7948d9156a879194d0c2deff851192da4cb6
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2021-10-03 20:28:44 -0700

    Updating lsp keybindings

--- ansible/roles/vim/files/init.lua
+++ ansible/roles/vim/files/init.lua
@@ -27,24 +27,25 @@ local on_attach = function(client, bufnr)
 
         -- TODO: Implement tab and vsplit
         -- nnoremap <silent> gt :call CocActionAsync('jumpDefinition', 'tabe')<cr>
-        -- nnoremap <silent> gv :call CocActionAsync('jumpDefinition', 'vsplit')<cr>
-        buf_set_keymap('n','gD'        ,'<cmd>lua vim.lsp.buf.declaration()<CR>'                               ,opts)
-        buf_set_keymap('n','gd'        ,'<cmd>lua vim.lsp.buf.definition()<CR>'                                ,opts)
-        buf_set_keymap('n','gk'        ,'<cmd>lua vim.lsp.buf.hover()<CR>'                                     ,opts)
-        buf_set_keymap('n','gi'        ,'<cmd>lua vim.lsp.buf.implementation()<CR>'                            ,opts)
-        buf_set_keymap('n','gK'        ,'<cmd>lua vim.lsp.buf.signature_help()<CR>'                            ,opts)
-        buf_set_keymap('n','<leader>la','<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>'                      ,opts)
-        buf_set_keymap('n','<leader>lr','<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>'                   ,opts)
-        buf_set_keymap('n','<leader>lw','<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',opts)
-        buf_set_keymap('n','gT'        ,'<cmd>lua vim.lsp.buf.type_definition()<CR>'                           ,opts)
-        buf_set_keymap('n','<leader>rn','<cmd>lua vim.lsp.buf.rename()<CR>'                                    ,opts)
-        buf_set_keymap('n','<leader>ca','<cmd>lua vim.lsp.buf.code_action()<CR>'                               ,opts)
-        buf_set_keymap('n','gr'        ,'<cmd>lua vim.lsp.buf.references()<CR>'                                ,opts)
-        buf_set_keymap('n','ge'        ,'<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>'              ,opts)
-        buf_set_keymap('n','[g'        ,'<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>'                          ,opts)
-        buf_set_keymap('n',']g'        ,'<cmd>lua vim.lsp.diagnostic.goto_next()<CR>'                          ,opts)
-        buf_set_keymap('n','<leader>lq','<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>'                        ,opts)
-        buf_set_keymap('n','<leader>lf','<cmd>lua vim.lsp.buf.formatting()<CR>'                                ,opts)
+        buf_set_keymap('n','gD'        ,'<cmd>lua vim.lsp.buf.declaration()<cr>'                               ,opts)
+        buf_set_keymap('n','gK'        ,'<cmd>lua vim.lsp.buf.signature_help()<cr>'                            ,opts)
+        buf_set_keymap('n','gR'        ,'<cmd>lua vim.lsp.buf.rename()<cr>'                                    ,opts)
+        buf_set_keymap('n','gT'        ,'<cmd>lua vim.lsp.buf.type_definition()<cr>'                           ,opts)
+        buf_set_keymap('n','ga'        ,'<cmd>lua vim.lsp.buf.code_action()<cr>'                               ,opts)
+        buf_set_keymap('n','gd'        ,'<cmd>lua vim.lsp.buf.definition()<cr>'                                ,opts)
+        buf_set_keymap('n','gi'        ,'<cmd>lua vim.lsp.buf.implementation()<cr>'                            ,opts)
+        buf_set_keymap('n','gk'        ,'<cmd>lua vim.lsp.buf.hover()<cr>'                                     ,opts)
+        buf_set_keymap('n','gr'        ,'<cmd>lua vim.lsp.buf.references()<cr>'                                ,opts)
+        buf_set_keymap('n','gv'        ,'<cmd>vsplit<cr><cmd>lua vim.lsp.buf.definition()<cr>'                 ,opts)
+        buf_set_keymap('n','gt'        ,'<cmd>tab split<cr><cmd>lua vim.lsp.buf.definition()<cr>'              ,opts)
+        buf_set_keymap('n','[g'        ,'<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>'                          ,opts)
+        buf_set_keymap('n',']g'        ,'<cmd>lua vim.lsp.diagnostic.goto_next()<cr>'                          ,opts)
+        buf_set_keymap('n','gla'       ,'<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>'                      ,opts)
+        buf_set_keymap('n','gld'       ,'<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>'              ,opts)
+        buf_set_keymap('n','glf'       ,'<cmd>lua vim.lsp.buf.formatting()<cr>'                                ,opts)
+        buf_set_keymap('n','glq'       ,'<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>'                        ,opts)
+        buf_set_keymap('n','glr'       ,'<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>'                   ,opts)
+        buf_set_keymap('n','glw'       ,'<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>',opts)
 
 end
 
@@ -60,27 +61,13 @@ cmp.setup({
                 { name = 'buffer' },
         },
         mapping = {
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping(function(fallback)
-                        if vim.fn.pumvisible() == 1 then
-                                if vim.fn["UltiSnips#CanExpandSnippet"]() == 1 then
-                                        return vim.fn.feedkeys(t("<C-R>=UltiSnips#ExpandSnippet()<CR>"))
-                                end
-                                vim.fn.feedkeys(t("<C-n>"), "n")
-                        elseif check_back_space() then
-                                vim.fn.feedkeys(t("<cr>"), "n")
-                        else
-                                fallback()
-                        end
-                end, {
-                                "i",
-                                "s",
-                        }),
+                ['<cr>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
                 ["<Tab>"] = cmp.mapping(function(fallback)
                         if vim.fn.complete_info()["selected"] == -1 and vim.fn["UltiSnips#CanExpandSnippet"]() == 1 then
-                                vim.fn.feedkeys(t("<C-R>=UltiSnips#ExpandSnippet()<CR>"))
+                                vim.fn.feedkeys(t("<C-R>=UltiSnips#ExpandSnippet()<cr>"))
                         elseif vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-                                vim.fn.feedkeys(t("<ESC>:call UltiSnips#JumpForwards()<CR>"))
+                                vim.fn.feedkeys(t("<ESC>:call UltiSnips#JumpForwards()<cr>"))
                         elseif vim.fn.pumvisible() == 1 then
                                 vim.fn.feedkeys(t("<C-n>"), "n")
                         elseif check_back_space() then
@@ -94,7 +81,7 @@ cmp.setup({
                         }),
                 ["<S-Tab>"] = cmp.mapping(function(fallback)
                         if vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
-                                return vim.fn.feedkeys(t("<C-R>=UltiSnips#JumpBackwards()<CR>"))
+                                return vim.fn.feedkeys(t("<C-R>=UltiSnips#JumpBackwards()<cr>"))
                         elseif vim.fn.pumvisible() == 1 then
                                 vim.fn.feedkeys(t("<C-p>"), "n")
                         else
@@ -130,6 +117,7 @@ end
 
 require('telescope').load_extension('fzf')
 require('gitsigns').setup()
+require('hop').setup()
 require('nvim-treesitter.configs').setup({
         ensure_installed = 'all',
         highlight = {
