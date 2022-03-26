dfe782a9433c9bab9359230de2ff7f0852629e55
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2022-03-26 16:53:03 -0700

    Increasing speed of neoscroll scroll speed

--- stow/nvim/.config/nvim/lua/aljendro/config/plugins/neoscroll.lua
+++ stow/nvim/.config/nvim/lua/aljendro/config/plugins/neoscroll.lua
@@ -6,7 +6,7 @@ t['<C-k>'] = {'scroll', {'-vim.wo.scroll', 'true', '100'}}
 t['<C-j>'] = {'scroll', {'vim.wo.scroll', 'true', '100'}}
 t['<M-k>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '200'}}
 t['<M-j>'] = {'scroll', {'vim.api.nvim_win_get_height(0)', 'true', '200'}}
-t['<Up>'] = {'scroll', {'-0.10', 'false', '100'}}
-t['<Down>'] = {'scroll', {'0.10', 'false', '100'}}
+t['<Up>'] = {'scroll', {'-0.10', 'false', '15'}}
+t['<Down>'] = {'scroll', {'0.10', 'false', '15'}}
 
 require('neoscroll.config').set_mappings(t)
