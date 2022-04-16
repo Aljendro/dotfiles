486c28a969bae551ee14c092dc366167de7e7c3e
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2022-04-16 13:25:49 -0700

    Updating auto-completion to be inline with api changes

--- stow/nvim/.config/nvim/lua/aljendro/config/auto-completion.lua
+++ stow/nvim/.config/nvim/lua/aljendro/config/auto-completion.lua
@@ -2,7 +2,7 @@ local cmp = require('cmp')
 local helper = require('aljendro/config/helper')
 
 cmp.setup({
-    experimental = {native_menu = false, ghost_text = true},
+    experimental = {ghost_text = true},
     formatting = {
         format = function(entry, vim_item)
             vim_item.menu = ({
@@ -21,16 +21,13 @@ cmp.setup({
             name = 'buffer',
             preselect = true,
             max_item_count = 20,
-            options = {keyword_pattern = [[\k\k\k\k\+]]}
+            option = {keyword_pattern = [[\k\k\k\+]]}
         }, {name = 'path'}
     }),
     mapping = {
         ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
         ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
-        ['<C-k>'] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close()
-        }),
+        ['<C-k>'] = cmp.mapping(cmp.mapping.close(), {'i', 'c'}),
         ['<C-j>'] = cmp.mapping.confirm({select = true}),
         ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
         ['<Tab>'] = cmp.mapping(function(fallback)
@@ -40,7 +37,7 @@ cmp.setup({
             elseif vim.fn['UltiSnips#CanJumpForwards']() == 1 then
                 vim.fn.feedkeys(helper.t(
                                     '<ESC>:call UltiSnips#JumpForwards()<cr>'))
-            elseif vim.fn.pumvisible() == 1 then
+            elseif cmp.visible() == 1 then
                 vim.fn.feedkeys(helper.t('<C-n>'), 'n')
             elseif helper.check_back_space() then
                 vim.fn.feedkeys(helper.t('<tab>'), 'n')
@@ -52,7 +49,7 @@ cmp.setup({
             if vim.fn['UltiSnips#CanJumpBackwards']() == 1 then
                 return vim.fn.feedkeys(helper.t(
                                            '<C-R>=UltiSnips#JumpBackwards()<cr>'))
-            elseif vim.fn.pumvisible() == 1 then
+            elseif cmp.visible() == 1 then
                 vim.fn.feedkeys(helper.t('<C-p>'), 'n')
             else
                 fallback()
