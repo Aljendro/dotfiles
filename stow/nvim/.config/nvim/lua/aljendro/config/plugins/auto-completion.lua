2d8ca319618ccada8856a1066786ad718084979a
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2022-07-09 20:03:46 -0700

    Checking cmp for nil

--- stow/nvim/.config/nvim/lua/aljendro/config/plugins/auto-completion.lua
+++ stow/nvim/.config/nvim/lua/aljendro/config/plugins/auto-completion.lua
@@ -1,68 +1,73 @@
 local cmp = require('cmp')
 local cmp_ultisnips = require("cmp_nvim_ultisnips.mappings")
 
-cmp.setup({
-    experimental = {ghost_text = true},
-    formatting = {
-        format = function(entry, vim_item)
-            vim_item.menu = ({
-                nvim_lsp = '[LSP]',
-                buffer = '[Buf]',
-                ultisnips = '[Ulti]',
-                dictionary = '[Dict]'
-            })[entry.source.name]
-            return vim_item
-        end
-    },
-    snippet = {expand = function(args) vim.fn['UltiSnips#Anon'](args.body) end},
-    sources = cmp.config.sources({
-        {name = 'nvim_lsp', preselect = true},
-        {name = 'ultisnips', preselect = true}, {
-            name = 'buffer',
-            preselect = true,
-            max_item_count = 20,
-            option = {keyword_pattern = [[\k\k\k\+]]}
-        }, {name = 'path'}
-    }),
-    mapping = {
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            cmp_ultisnips.compose {"expand", "jump_forwards"}(fallback)
-        end, {'i', 's'}),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            cmp_ultisnips.compose {"jump_backwards"}(fallback)
-        end, {'i', 's'}),
-        ['<M-j>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i'}),
-        ['<M-k>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i'}),
-        ['<C-j>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item({behavior = cmp.SelectBehavior.Select})
-            else
-                fallback()
+if (cmp) then
+    cmp.setup({
+        experimental = {ghost_text = true},
+        formatting = {
+            format = function(entry, vim_item)
+                vim_item.menu = ({
+                    nvim_lsp = '[LSP]',
+                    buffer = '[Buf]',
+                    ultisnips = '[Ulti]',
+                    dictionary = '[Dict]'
+                })[entry.source.name]
+                return vim_item
             end
-        end, {'i'}),
-        ['<C-k>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item({behavior = cmp.SelectBehavior.Select})
-            else
-                fallback()
+        },
+        snippet = {
+            expand = function(args)
+                vim.fn['UltiSnips#Anon'](args.body)
             end
-        end, {'i'}),
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i'}),
-        ['<C-e>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.mapping.abort()
-                cmp.mapping.close()
-            else
-                fallback()
-            end
-        end, {'i'}),
-        ['<CR>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.confirm({select = true})
-            else
-                fallback()
-            end
-        end, {'i'})
-    }
-})
-
+        },
+        sources = cmp.config.sources({
+            {name = 'nvim_lsp', preselect = true},
+            {name = 'ultisnips', preselect = true}, {
+                name = 'buffer',
+                preselect = true,
+                max_item_count = 20,
+                option = {keyword_pattern = [[\k\k\k\+]]}
+            }, {name = 'path'}
+        }),
+        mapping = {
+            ['<Tab>'] = cmp.mapping(function(fallback)
+                cmp_ultisnips.compose {"expand", "jump_forwards"}(fallback)
+            end, {'i', 's'}),
+            ['<S-Tab>'] = cmp.mapping(function(fallback)
+                cmp_ultisnips.compose {"jump_backwards"}(fallback)
+            end, {'i', 's'}),
+            ['<M-j>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i'}),
+            ['<M-k>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i'}),
+            ['<C-j>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item({behavior = cmp.SelectBehavior.Select})
+                else
+                    fallback()
+                end
+            end, {'i'}),
+            ['<C-k>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item({behavior = cmp.SelectBehavior.Select})
+                else
+                    fallback()
+                end
+            end, {'i'}),
+            ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i'}),
+            ['<C-e>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.mapping.abort()
+                    cmp.mapping.close()
+                else
+                    fallback()
+                end
+            end, {'i'}),
+            ['<CR>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.confirm({select = true})
+                else
+                    fallback()
+                end
+            end, {'i'})
+        }
+    })
+end
