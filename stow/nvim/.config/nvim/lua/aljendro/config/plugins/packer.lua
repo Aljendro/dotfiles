996d592431a8333c1df70c5054e0e2dd05ecf7b5
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2022-09-16 10:39:01 -0700

    Using different autosave plugin

--- stow/nvim/.config/nvim/lua/aljendro/config/plugins/packer.lua
+++ stow/nvim/.config/nvim/lua/aljendro/config/plugins/packer.lua
@@ -10,8 +10,27 @@ return require('packer').startup(function()
         config = function() require('neoclip').setup({}) end
     }
     use {
-        "Pocco81/auto-save.nvim",
-        config = function() require('auto-save').setup({}) end
+        "doubleloop/auto-save.nvim",
+        commit = "2f52da487b26a2eafed289ff78a6e8df52995f6f",
+        config = function()
+            require('auto-save').setup({
+                trigger_events = {"InsertLeave", "TextChanged"}, -- vim events that trigger auto-save. See :h events
+                -- function that determines whether to save the current buffer or not
+                -- return true: if buffer is ok to be saved
+                -- return false: if it's not ok to be saved
+                condition = function(buf)
+                    local fn = vim.fn
+                    local utils = require("auto-save.utils.data")
+
+                    if fn.getbufvar(buf, "&modifiable") == 1 and
+                        utils.not_in(fn.getbufvar(buf, "&filetype"),
+                                     {'TelescopePrompt', 'TelescopeResults'}) then
+                        return true -- met condition(s), can save
+                    end
+                    return false -- can't save
+                end
+            })
+        end
     }
     use {
         'Pocco81/DAPInstall.nvim',
