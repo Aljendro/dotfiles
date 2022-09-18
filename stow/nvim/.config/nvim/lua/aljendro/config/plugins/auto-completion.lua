d941addab3c28a8d15931d631ee28b1ff6b56164
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2022-09-17 23:01:26 -0700

    Updating nvim-cmp configuration

--- stow/nvim/.config/nvim/lua/aljendro/config/plugins/auto-completion.lua
+++ stow/nvim/.config/nvim/lua/aljendro/config/plugins/auto-completion.lua
@@ -1,28 +1,138 @@
 local cmp = require('cmp')
 local cmp_ultisnips = require("cmp_nvim_ultisnips.mappings")
 
+local kind_icons = {
+    Class = "ﴯ",
+    Color = "",
+    Constant = "",
+    Constructor = "",
+    Enum = "",
+    EnumMember = "",
+    Event = "",
+    Field = "",
+    File = "",
+    Folder = "",
+    Function = "",
+    Interface = "",
+    Keyword = "",
+    Method = "",
+    Module = "",
+    Operator = "",
+    Property = "ﰠ",
+    Reference = "",
+    Snippet = "",
+    Struct = "",
+    Text = "",
+    TypeParameter = "",
+    Unit = "",
+    Value = "",
+    Variable = ""
+}
+
+local lspkind_comparator = function(conf)
+    local lsp_types = require('cmp.types').lsp
+    return function(entry1, entry2)
+        if entry1.source.name ~= 'nvim_lsp' then
+            if entry2.source.name == 'nvim_lsp' then
+                return false
+            else
+                return nil
+            end
+        end
+        local kind1 = lsp_types.CompletionItemKind[entry1:get_kind()]
+        local kind2 = lsp_types.CompletionItemKind[entry2:get_kind()]
+
+        local priority1 = conf.kind_priority[kind1] or 0
+        local priority2 = conf.kind_priority[kind2] or 0
+        if priority1 == priority2 then return nil end
+        return priority2 < priority1
+    end
+end
+
+local label_comparator = function(entry1, entry2)
+    return entry1.completion_item.label < entry2.completion_item.label
+end
+
 cmp.setup({
     experimental = {ghost_text = true},
     formatting = {
         format = function(entry, vim_item)
+            vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind],
+                                          vim_item.kind) -- Concatonate the icons with name of the item-kind
             vim_item.menu = ({
                 nvim_lsp = '[LSP]',
-                buffer = '[Buf]',
-                ultisnips = '[Ulti]',
-                dictionary = '[Dict]'
+                spell = '[Spellings]',
+                zsh = '[Zsh]',
+                buffer = '[Buffer]',
+                ultisnips = '[Snip]',
+                treesitter = '[Treesitter]',
+                calc = '[Calculator]',
+                nvim_lua = '[Lua]',
+                path = '[Path]',
+                nvim_lsp_signature_help = '[Signature]',
+                cmdline = '[Vim Command]'
             })[entry.source.name]
             return vim_item
         end
     },
+    comparators = {
+        lspkind_comparator({
+            kind_priority = {
+                Field = 11,
+                Property = 11,
+                Enum = 10,
+                EnumMember = 10,
+                Event = 10,
+                Function = 10,
+                Method = 10,
+                Operator = 10,
+                Reference = 10,
+                Struct = 10,
+                Constant = 9,
+                Variable = 9,
+                File = 8,
+                Folder = 8,
+                Class = 5,
+                Color = 5,
+                Module = 5,
+                Keyword = 2,
+                Constructor = 1,
+                Interface = 1,
+                Snippet = 1,
+                Text = 1,
+                TypeParameter = 1,
+                Unit = 1,
+                Value = 1
+            }
+        }), label_comparator
+    },
     snippet = {expand = function(args) vim.fn['UltiSnips#Anon'](args.body) end},
+    matching = {disallow_fuzzy_matching = false},
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered()
+    },
     sources = cmp.config.sources({
-        {name = 'nvim_lsp', preselect = true},
-        {name = 'ultisnips', preselect = true}, {
+        {
+            name = 'nvim_lsp',
+            preselect = true,
+            keyword_length = 2,
+            group_index = 1
+        },
+        {
+            name = 'ultisnips',
+            preselect = true,
+            keyword_length = 2,
+            group_index = 1
+        }, {
             name = 'buffer',
             preselect = true,
+            keyword_length = 4,
             max_item_count = 20,
-            option = {keyword_pattern = [[\k\k\k\+]]}
-        }, {name = 'path'}
+            option = {keyword_pattern = [[\k\k\k\+]]},
+            group_index = 2
+        }, {name = 'nvim_lsp_signature_help', group_index = 3},
+        {name = 'path', group_index = 4}
     }),
     mapping = {
         ['<Tab>'] = cmp.mapping(function(fallback)
@@ -39,27 +149,36 @@ cmp.setup({
             else
                 fallback()
             end
-        end, {'i'}),
+        end, {'i', 'c'}),
         ['<C-k>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item({behavior = cmp.SelectBehavior.Select})
             else
                 fallback()
             end
-        end, {'i'}),
+        end, {'i', 'c'}),
         ['<C-Space>'] = cmp.mapping(function()
             if cmp.visible() then
                 cmp.close()
             else
                 cmp.complete()
             end
-        end, {'i'}),
+        end, {'i', 'c'}),
         ['<CR>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.confirm({select = true})
             else
                 fallback()
             end
-        end, {'i'})
+        end, {'i', 'c'})
     }
 })
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        {name = 'path', keyword_length = 3, group_index = 1},
+        {name = 'cmdline', keyword_length = 3, group_index = 2}
+    })
+})
+
