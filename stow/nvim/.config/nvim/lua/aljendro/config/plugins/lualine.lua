6b3da36a499af87015577f46ef99f4f907e5d1e8
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2022-10-03 08:15:24 -0700

    Updating for neovim 0.8

--- stow/nvim/.config/nvim/lua/aljendro/config/plugins/lualine.lua
+++ stow/nvim/.config/nvim/lua/aljendro/config/plugins/lualine.lua
@@ -1,6 +1,6 @@
 require('lualine').setup({
     options = {theme = 'tokyonight', globalstatus = true},
-    sections = {lualine_c = {{'filename', path = 1}}},
+    sections = {lualine_c = {}},
     tabline = {
         lualine_a = {{'buffers', mode = 0}},
         lualine_b = {},
@@ -8,5 +8,21 @@ require('lualine').setup({
         lualine_x = {},
         lualine_y = {},
         lualine_z = {'tabs'}
+    },
+    winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {{'filename', path = 1}},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
+    },
+    inactive_winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {{'filename', path = 1}},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
     }
 })
