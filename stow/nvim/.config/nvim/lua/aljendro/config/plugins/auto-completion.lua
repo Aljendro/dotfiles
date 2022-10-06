1999042fa4d356dfcc6ed1a8224b7d9e2f89ac57
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2022-10-06 16:22:14 -0700

    Disabling custom comparator in nvim-cmp for now

--- stow/nvim/.config/nvim/lua/aljendro/config/plugins/auto-completion.lua
+++ stow/nvim/.config/nvim/lua/aljendro/config/plugins/auto-completion.lua
@@ -29,34 +29,34 @@ local kind_icons = {
     Variable = ""
 }
 
-local lspkind_comparator = function(conf)
-    local lsp_types = require('cmp.types').lsp
-    return function(entry1, entry2)
-        if entry1.source.name ~= 'nvim_lsp' then
-            if entry2.source.name == 'nvim_lsp' then
-                return false
-            else
-                return nil
-            end
-        end
-        local kind1 = lsp_types.CompletionItemKind[entry1:get_kind()]
-        local kind2 = lsp_types.CompletionItemKind[entry2:get_kind()]
-
-        local priority1 = conf.kind_priority[kind1] or 0
-        local priority2 = conf.kind_priority[kind2] or 0
-        if priority1 == priority2 then return nil end
-        return priority2 < priority1
-    end
-end
-
-local label_comparator = function(entry1, entry2)
-    return entry1.completion_item.label < entry2.completion_item.label
-end
+-- local lspkind_comparator = function(conf)
+--     local lsp_types = require('cmp.types').lsp
+--     return function(entry1, entry2)
+--         if entry1.source.name ~= 'nvim_lsp' then
+--             if entry2.source.name == 'nvim_lsp' then
+--                 return false
+--             else
+--                 return nil
+--             end
+--         end
+--         local kind1 = lsp_types.CompletionItemKind[entry1:get_kind()]
+--         local kind2 = lsp_types.CompletionItemKind[entry2:get_kind()]
+--
+--         local priority1 = conf.kind_priority[kind1] or 0
+--         local priority2 = conf.kind_priority[kind2] or 0
+--         if priority1 == priority2 then return nil end
+--         return priority2 < priority1
+--     end
+-- end
+--
+-- local label_comparator = function(entry1, entry2)
+--     return entry1.completion_item.label < entry2.completion_item.label
+-- end
 
 cmp.setup({
     experimental = {ghost_text = true},
     formatting = {
-        format = function(entry, vim_item)
+        format = function(_, vim_item)
             vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind],
                                           vim_item.kind) -- Concatonate the icons with name of the item-kind
 
@@ -64,45 +64,45 @@ cmp.setup({
             return vim_item
         end
     },
-    sorting = {
-        comparators = {
-            cmp.config.compare.score, cmp.config.compare.offset, -- cmp.config.compare.exact,
-            -- cmp.config.compare.kind, -- cmp.config.compare.sort_text,
-            -- cmp.config.compare.length
-            -- cmp.config.compare.order
-        }
-    },
-    comparators = {
-        lspkind_comparator({
-            kind_priority = {
-                Field = 11,
-                Property = 11,
-                Enum = 10,
-                EnumMember = 10,
-                Event = 10,
-                Function = 10,
-                Method = 10,
-                Operator = 10,
-                Reference = 10,
-                Struct = 10,
-                Constant = 9,
-                Variable = 9,
-                File = 8,
-                Folder = 8,
-                Class = 5,
-                Color = 5,
-                Module = 5,
-                Keyword = 2,
-                Constructor = 1,
-                Interface = 1,
-                Snippet = 1,
-                Text = 1,
-                TypeParameter = 1,
-                Unit = 1,
-                Value = 1
-            }
-        }), label_comparator
-    },
+    -- sorting = {
+    --     comparators = {
+    --         cmp.config.compare.score, cmp.config.compare.offset, -- cmp.config.compare.exact,
+    --         -- cmp.config.compare.kind, -- cmp.config.compare.sort_text,
+    --         -- cmp.config.compare.length
+    --         -- cmp.config.compare.order
+    --     }
+    -- },
+    -- comparators = {
+    --     lspkind_comparator({
+    --         kind_priority = {
+    --             Field = 11,
+    --             Property = 11,
+    --             Enum = 10,
+    --             EnumMember = 10,
+    --             Event = 10,
+    --             Function = 10,
+    --             Method = 10,
+    --             Operator = 10,
+    --             Reference = 10,
+    --             Struct = 10,
+    --             Constant = 9,
+    --             Variable = 9,
+    --             File = 8,
+    --             Folder = 8,
+    --             Class = 5,
+    --             Color = 5,
+    --             Module = 5,
+    --             Keyword = 2,
+    --             Constructor = 1,
+    --             Interface = 1,
+    --             Snippet = 1,
+    --             Text = 1,
+    --             TypeParameter = 1,
+    --             Unit = 1,
+    --             Value = 1
+    --         }
+    --     }), label_comparator
+    -- },
     snippet = {expand = function(args) vim.fn['UltiSnips#Anon'](args.body) end},
     matching = {disallow_fuzzy_matching = false},
     window = {
