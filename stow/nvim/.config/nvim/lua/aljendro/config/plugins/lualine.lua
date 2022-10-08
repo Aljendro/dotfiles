95ded637984ddf4434efd7296ddb851f801b4fa1
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2022-10-08 09:41:54 -0700

    Adding statuline lsp progress

--- stow/nvim/.config/nvim/lua/aljendro/config/plugins/lualine.lua
+++ stow/nvim/.config/nvim/lua/aljendro/config/plugins/lualine.lua
@@ -1,6 +1,6 @@
 require('lualine').setup({
     options = {theme = 'tokyonight', globalstatus = true},
-    sections = {lualine_c = {{'filename', path = 1}}},
+    sections = {lualine_c = {{'filename', path = 1}}, lualine_x = {'lsp_progress', 'encoding', 'fileformat', 'filetype'}},
     tabline = {
         lualine_a = {{'buffers', mode = 0}},
         lualine_b = {},
