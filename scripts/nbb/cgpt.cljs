6735c17947cb4dfd0a9d673f7c4745633b8d3449
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2023-04-17 21:47:02 -0700

    update: adding comment block to pull all open ai models

--- scripts/nbb/cgpt.cljs
+++ scripts/nbb/cgpt.cljs
@@ -17,6 +17,9 @@
 ;; Helper Functions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn request-models []
+  (.listModels openai-api))
+
 (defn request-completion [query & {:keys [model context] :or {model "gpt-3.5-turbo" context []}}]
   (.. openai-api
       (createChatCompletion
@@ -26,6 +29,26 @@
 (defn get-response-content [response]
   (let [response (js->clj response)]
     (get-in response ["data" "choices" 0 "message" "content"])))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Repl Usage
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(comment
+  ; Get a list of the latest models newest to oldest
+  (do
+    (require '[cljs.pprint :refer [pprint]])
+    #_{:clj-kondo/ignore [:unresolved-symbol]}
+    (p/let [response (request-models)
+            models (js->clj response)]
+      (->>
+       ; Get the list of models
+       (get-in models ["data" "data"])
+       ; only take the id and created fields
+       (map #(select-keys % ["id" "created"]))
+       ; sort newest to oldest
+       (sort #(compare (get %2 "created") (get %1 "created")));
+       (pprint)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Main
