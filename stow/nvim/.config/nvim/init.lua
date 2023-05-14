0f65cf81fc37f2c15db92d2260209608d1ac5146
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2023-05-14 13:52:17 -0700

    update: translating autocommands from vimscript to lua

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -185,27 +185,45 @@ require('packer').startup(function()
     }
 end)
 
-vim.cmd([[
-
-augroup customVim
-      autocmd!
-      " When editing a file, always jump to the last known cursor position.
-      autocmd BufReadPost *
-                        \ if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit'
-                        \ |   exe "normal! g`\""
-                        \ | endif
-      " Create a default session when vim leaves
-      autocmd VimLeave * :call v:lua.MakeSession('default')
-      " Autosave when files change text
-      autocmd TextChanged,TextChangedI *.txt,*.sh,*.md,*.html,*.yml,*.yaml,*.css,*.js,*.ts,*.jsx,*.tsx,*.json,*.jsonl,*.py,*.rs,*.go,*.lua,*.fnl,*.clj,*.cljs,*.cljc silent! w
-      " Open quickfix after command that populates it is run
-      autocmd QuickFixCmdPost [^l]* cwindow
-      autocmd QuickFixCmdPost l* lwindow
-      " Highlight the movement selection
-      autocmd TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=150 }
-augroup END
-
-]])
+local customVim = vim.api.nvim_create_augroup('customVim', {clear = true})
+vim.api.nvim_create_autocmd({'BufReadPost'}, {
+    group = customVim,
+    pattern = '*',
+    callback = function()
+        vim.cmd([[
+            if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit'
+            \|   exe "normal! g`\""
+            \| endif
+        ]])
+    end
+})
+vim.api.nvim_create_autocmd({'VimLeave'}, {
+    group = customVim,
+    pattern = '*',
+    callback = function() MakeSession('default') end
+})
+vim.api.nvim_create_autocmd({'TextChanged', 'TextChangedI'}, {
+    group = customVim,
+    pattern = '*.txt,*.sh,*.md,*.html,*.yml,*.yaml,*.css,*.js,*.ts,*.jsx,*.tsx,*.json,*.jsonl,*.py,*.rs,*.go,*.lua,*.fnl,*.clj,*.cljs,*.cljc',
+    callback = function() vim.cmd("silent! w") end
+})
+vim.api.nvim_create_autocmd({'QuickFixCmdPost'}, {
+    group = customVim,
+    pattern = '[^l]*',
+    callback = function() vim.cmd("cwindow") end
+})
+vim.api.nvim_create_autocmd({'QuickFixCmdPost'}, {
+    group = customVim,
+    pattern = 'l*',
+    callback = function() vim.cmd("lwindow") end
+})
+vim.api.nvim_create_autocmd({'TextYankPost'}, {
+    group = customVim,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank {higroup = 'IncSearch', timeout = 150}
+    end
+})
 
 -- Enable Aniseed's automatic compilation and loading of Fennel source code.
 vim.g["aniseed#env"] = {module = "dotfiles.init", compile = true}
