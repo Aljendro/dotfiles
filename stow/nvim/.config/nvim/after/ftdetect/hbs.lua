a0e35c0eba4f7f2d66b8a66159e4439f53162c1f
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2023-08-08 12:21:40 -0400

    update: force handlebar templates to use html as the filetype

---
+++ stow/nvim/.config/nvim/after/ftdetect/hbs.lua
@@ -0,0 +1,9 @@
+vim.api.nvim_create_autocmd(
+    { "BufRead", "BufNewFile" },
+    {
+        pattern = "*.hbs",
+        callback = function()
+            vim.bo.filetype = "html"
+        end,
+    }
+)
