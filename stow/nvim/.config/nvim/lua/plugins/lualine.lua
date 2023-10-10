6172b587d392a616fcbb57c51642bd0bdf44d604
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2023-10-09 19:11:46 -0700

    update: updating neovim ui bufferline,tabline, symbols

--- stow/nvim/.config/nvim/lua/plugins/lualine.lua
+++ stow/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -3,14 +3,24 @@ return {
     config = function()
         require("lualine").setup {
             options = { theme = "tokyonight", globalstatus = true },
-            sections = { lualine_c = { { "filename", path = 1 } }, lualine_x = { "encoding", "fileformat", "filetype" } },
+            sections = {
+                lualine_b = { 'branch', 'diff', {
+                    'diagnostics',
+                    always_visible = false,
+                    sections = { 'error', 'warn', 'info', 'hint' },
+                    sources = { 'nvim_diagnostic' },
+                    symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
+                }
+                },
+                lualine_c = { { "filename", path = 1, file_status = true, symbols = { modified = '✏️' } } }
+            },
             tabline = {
-                lualine_a = { { "buffers", mode = 4 } },
+                lualine_a = { { "tabs", use_mode_colors = true } },
                 lualine_b = {},
                 lualine_c = {},
                 lualine_x = {},
                 lualine_y = {},
-                lualine_z = { "tabs" },
+                lualine_z = {},
             },
         }
     end,
--- stow/nvim/.config/nvim/lua/settings.lua
+++ stow/nvim/.config/nvim/lua/settings.lua
@@ -39,6 +39,11 @@ vim.opt.virtualedit = { "block", "onemore" }
 vim.opt.wrap = false
 vim.opt.writebackup = false
 
+vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
+vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
+
 vim.cmd([[
 let g:copilot_no_tab_map = v:true
 let g:VM_theme = "neon"
