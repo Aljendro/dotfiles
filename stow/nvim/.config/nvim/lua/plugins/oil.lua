18ee3fca9b84490ba01961c3c3c3b6c8b0b70897
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2023-10-12 20:51:00 -0700

    update: adding config for oil.nvim plugin

--- stow/nvim/.config/nvim/lua/plugins/oil.lua
+++ stow/nvim/.config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,17 @@ return {
   'stevearc/oil.nvim',
   opts = {},
   config = function()
-    require("oil").setup()
+    require("oil").setup({
+      columns = {
+        "permissions",
+        "size",
+        { "mtime", format = "%Y-%m-%d %X" },
+        { "icon",  add_padding = false },
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    })
 
     local c = require("common")
     c.kmap("n", "-", "<cmd>Oil<cr>")
