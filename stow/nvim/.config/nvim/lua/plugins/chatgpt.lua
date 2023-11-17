1552b5d0b80acb4ff324c01f27626887dbf020d5
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2023-11-17 05:03:21 -0800

    update: increasing size of chatgpt payloads

--- stow/nvim/.config/nvim/lua/plugins/chatgpt.lua
+++ stow/nvim/.config/nvim/lua/plugins/chatgpt.lua
@@ -42,10 +42,10 @@ return {
                 },
             },
             openai_params = {
-                model = "gpt-3.5-turbo",
+                model = "gpt-3.5-turbo-16k",
                 frequency_penalty = 0,
                 presence_penalty = 0,
-                max_tokens = 300,
+                max_tokens = 3000,
                 temperature = 0,
                 top_p = 1,
                 n = 1,
