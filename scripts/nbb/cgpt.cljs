ae5112ad3d55c457b24cd2bd305153c4017c740e
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2024-09-01 16:10:00 -0700

    feat: upgrading openai version to latest

--- scripts/nbb/cgpt.cljs
+++ scripts/nbb/cgpt.cljs
@@ -10,45 +10,27 @@
 
 (.config dotenv)
 ;; Setup openai configuration and api instance
-(def configuration (new (.-Configuration openai) (clj->js {:apiKey (.-OPENAI_API_KEY (.-env js/process))})))
-(def openai-api (new (.-OpenAIApi openai) configuration))
+(def openai-api (new (.-OpenAI openai) 
+                     (clj->js {:apiKey (.-OPENAI_API_KEY (.-env js/process))})))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Helper Functions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn request-models []
-  (.listModels openai-api))
+  (.. openai-api -models list))
 
 (defn request-completion [query & {:keys [model context] :or {model "gpt-3.5-turbo" context []}}]
   (.. openai-api
-      (createChatCompletion
+      -chat
+      -completions
+      (create
        (clj->js {:model model
                  :messages (concat context [{:role "user" :content query}])}))))
 
 (defn get-response-content [response]
-  (let [response (js->clj response)]
-    (get-in response ["data" "choices" 0 "message" "content"])))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Repl Usage
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(comment
-  ; Get a list of the latest models newest to oldest
-  (do
-    (require '[cljs.pprint :refer [pprint]])
-    #_{:clj-kondo/ignore [:unresolved-symbol]}
-    (p/let [response (request-models)
-            models (js->clj response)]
-      (->>
-       ; Get the list of models
-       (get-in models ["data" "data"])
-       ; only take the id and created fields
-       (map #(select-keys % ["id" "created"]))
-       ; sort newest to oldest
-       (sort #(compare (get %2 "created") (get %1 "created")));
-       (pprint)))))
+  (let [response (js->clj (.-choices response))]
+    (get-in response [0 "message" "content"])))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Main
@@ -62,3 +44,28 @@
     #_{:clj-kondo/ignore [:unresolved-symbol]}
     (p/let [response (request-completion input)]
       (println (get-response-content response)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Repl Usage
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(comment
+  ; Get a list of the latest models newest to oldest
+  (do
+    (require '[cljs.pprint :refer [pprint]])
+    #_{:clj-kondo/ignore [:unresolved-symbol]}
+    (p/let [response (request-models)]
+      (let [models (js->clj (.-data response))]
+        (->>
+         models
+         ; only take the id and created fields
+         (map #(select-keys % ["id" "created"]))
+         ; sort newest to oldest
+         (sort #(compare (get %2 "created") (get %1 "created")));
+         (pprint))))))
+
+(comment
+  ; run a simple completion
+  #_{:clj-kondo/ignore [:unresolved-symbol]}
+  (p/let [response (request-completion "What is the capital of France?")]
+    (println (get-response-content response))))
