5d102022f992a1c298838bf09fa0e583d6499f30
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2024-09-02 13:49:10 -0700

    feat: replacing null-ls with conform and nvim-lint

---
+++ stow/nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,31 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
+
+        conform.setup({
+            formatters_by_ft = {
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                css = { "prettier" },
+                html = { "prettier" },
+                json = { "prettier" },
+                yaml = { "prettier" },
+                markdown = { "prettier" },
+                graphql = { "prettier" },
+                lua = { "stylua" },
+            },
+        })
+
+        vim.keymap.set({ "n", "v" }, "glf", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            })
+        end)
+    end,
+}
