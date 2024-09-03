e6e4b23c38934d3fb89e6b23e642fadcd517588e
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2024-09-02 20:21:56 -0700

    chore: remove package-lock generation for plugin

--- stow/nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ stow/nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,7 +1,7 @@
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
+    build = "cd app && npm install --no-package-lock",
     init = function()
         vim.g.mkdp_filetypes = { "markdown" }
     end,
