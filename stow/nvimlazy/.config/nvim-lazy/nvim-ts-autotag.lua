0938b37641ac5b3d0a86e0d316674eecc7304cdc
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2024-09-08 11:02:41 -0700

    feat: removing nvim lazy

--- stow/nvimlazy/.config/nvim-lazy/nvim-ts-autotag.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "windwp/nvim-ts-autotag",
-    ft = { "javascriptreact", "typescriptreact", "html" },
-    config = function()
-        require("nvim-ts-autotag").setup({})
-    end,
-}
