6753b7ba379b4bddb322d6cfc9e067f87d92e3b3
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2025-11-17 19:45:55 -0800

    feat: removing babashka scripting

--- scripts/bb/src/aljendro/gen/actions/fns.clj
+++
@@ -1,8 +0,0 @@
-(ns aljendro.gen.actions.fns)
-
-(defmulti gen-fns (fn [fn-kw _input _context] fn-kw))
-
-(defmethod gen-fns :default [fn-kw _input _context]
-  (throw (IllegalArgumentException.
-          (str "Unknown gen function: " fn-kw))))
-
