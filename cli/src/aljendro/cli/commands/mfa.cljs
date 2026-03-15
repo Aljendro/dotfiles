aa75c3d87b5335d4f98287bce2d74b5d439f370d
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2026-03-15 15:53:34 -0700

    fix: fixing type inference issues

--- cli/src/aljendro/cli/commands/mfa.cljs
+++ cli/src/aljendro/cli/commands/mfa.cljs
@@ -3,7 +3,7 @@
             ["@smithy/shared-ini-file-loader" :refer [loadSharedConfigFiles]]
             ["@inquirer/prompts" :refer [input]]
             ["node:child_process" :refer [execSync]]
-            [cljs.core.async :refer [<!]]
+            [cljs.core.async :refer [go <!]]
             [cljs.core.async.interop :refer-macros [<p!]]
             [aljendro.cli.utils.common :refer-macros [adefn adefn-]]))
 
@@ -16,18 +16,18 @@
 
 (adefn- get-session [mfa-serial mfa-code]
   (try
-    (<p! (.send sts-client
-                (GetSessionTokenCommand.
-                 #js {:TokenCode       mfa-code
-                      :SerialNumber    mfa-serial
-                      :DurationSeconds session-duration})))
+    (js->clj (<p! (.send sts-client
+                         (GetSessionTokenCommand.
+                          #js {:TokenCode       mfa-code
+                               :SerialNumber    mfa-serial
+                               :DurationSeconds session-duration}))))
     (catch js/Error e
       (println "ERROR: Unable to get session token:" (.-message e))
       (js/process.exit 1))))
 
 (adefn- prompt-for-token [profile]
   (try
-    (<p! (input #js {:message (str "MFA Code for profile (" profile "): ")}))
+    (js->clj (<p! (input #js {:message (str "MFA Code for profile (" profile "): ")})))
     (catch js/Error _
       (println "INFO: Input was closed")
       (js/process.exit 1))))
@@ -35,11 +35,11 @@
 (adefn- refresh-token [profile mfa-serial mfa-code]
   (try
     (let [session (<! (get-session mfa-serial mfa-code))
-          creds   (.-Credentials session)
-          key-id  (.-AccessKeyId creds)
-          expiry  (.toISOString (.-Expiration creds))
-          secret  (.-SecretAccessKey creds)
-          token   (.-SessionToken creds)]
+          creds   (get session "Credentials")
+          key-id  (get creds "AccessKeyId")
+          expiry  (.toISOString (get creds "Expiration"))
+          secret  (get creds "SecretAccessKey")
+          token   (get creds "SessionToken")]
       (execSync (str "aws configure set session_expiration " expiry " --profile " profile))
       (execSync (str "aws configure set aws_access_key_id " key-id " --profile " profile))
       (execSync (str "aws configure set aws_secret_access_key " secret " --profile " profile))
@@ -53,14 +53,13 @@
     (when-not profile
       (println "ERROR: profile argument is required")
       (js/process.exit 1))
-    (let [config         (<p! (loadSharedConfigFiles))
-          profile-config (aget (.-configFile config) profile)
-          expiration     (or (and profile-config (.-session_expiration profile-config)) default-date)]
+    (let [config         (js->clj (<p! (loadSharedConfigFiles)))
+          profile-config (get-in config ["configFile" profile])
+          expiration     (get profile-config "session_expiration" default-date)]
       (if (session-expired? expiration)
         (let [mfa-code     (<! (prompt-for-token profile))
-              creds-config (aget (.-credentialsFile config) profile)
-              mfa-serial   (or (and creds-config (.-mfa_serial creds-config)) "")]
+              creds-config (get-in config ["credentialsFile" profile])
+              mfa-serial   (get creds-config "mfa_serial" "")]
           (<! (refresh-token profile mfa-serial mfa-code)))
         (println (str "INFO: Token is still valid with expiration (" expiration ")"))))))
-
 
