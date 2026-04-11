3d5928a718f9ca6866b9fba0139120787afd6ac5
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2026-04-11 13:02:04 -0700

    feat: fixing do droplet creation

--- cli/src/aljendro/cli/commands/ui/fleet/agent.cljs
+++ cli/src/aljendro/cli/commands/ui/fleet/agent.cljs
@@ -25,7 +25,7 @@
                    :digitalocean
                    (-> (remote/digitalocean-start! digitalocean-name)
                        (.then (fn [ip]
-                                (let [host (str "dev@" (.trim ip))]
+                                (let [host (str "root@" (.trim ip))]
                                   (reset! digitalocean-host-atom host)
                                   (update-agent! id #(assoc % :digitalocean-host host))
                                   (remote/rsync-to-digitalocean! host branch)))))
